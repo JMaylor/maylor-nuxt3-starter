ad6b53dc4d7b418a105a665affa2d207a4cee409
joemaylor <77979674+joemaylor@users.noreply.github.com>
2023-09-11 16:38:42 +0100

    add pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,6 @@ export default defineNuxtConfig({
   modules: [
     '@vueuse/nuxt', // https://vueuse.org/nuxt/README.html
     '@nuxtjs/tailwindcss', // https://tailwindcss.nuxtjs.org/
+    '@pinia/nuxt', // https://pinia.vuejs.org/ssr/nuxt.html
   ],
 })
