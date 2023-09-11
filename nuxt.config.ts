10aa1d5fddf27d7339ad220619d26b360d0285ff
joemaylor <77979674+joemaylor@users.noreply.github.com>
2023-09-11 16:45:40 +0100

    add and configure eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
     '@vueuse/nuxt', // https://vueuse.org/nuxt/README.html
     '@nuxtjs/tailwindcss', // https://tailwindcss.nuxtjs.org/
     '@pinia/nuxt', // https://pinia.vuejs.org/ssr/nuxt.html
-    'nuxt-icon' // https://github.com/nuxt-modules/icon
+    'nuxt-icon', // https://github.com/nuxt-modules/icon
+    '@nuxtjs/eslint-module', // https://nuxt.com/modules/eslint
   ],
 })
