7f1aa4cef141ccbc1e5a657781160563acbf2889
Joe Maylor <JMaylor@albacorecapital.com>
2023-09-18 16:17:00 +0100

    chore: fix TOOD: comment

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,5 @@
 export default defineAppConfig({
-  // TODO - this is a temporary fix as per https://github.com/nuxt-modules/icon/issues/86#issuecomment-1656960233
+  // TODO: this is a temporary fix as per https://github.com/nuxt-modules/icon/issues/86#issuecomment-1656960233
   nuxtIcon: {
     aliases: undefined,
     class: undefined,
